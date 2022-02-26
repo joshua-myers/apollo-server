e9f1e16a388046dc703d02a8cb583734db5f7fc3
joshua-myers <100009956+joshua-myers@users.noreply.github.com>
2022-02-25 20:11:04 -0600

    Back to square one - Clear out user model

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,21 +1,6 @@
 const resolvers = {
-  Query: {
-    async user(root, { id }, { models }) {
-      return models.User.findByPk(id);
-    },
-    async users(root, args, { models }) {
-      return models.User.findAll();
-    }
-  },
-  Mutation: {
-    async createUser(root, { firstName, lastName, email }, { models }) {
-      return models.User.create({
-        firstName,
-        lastName,
-        email
-      });
-    }
-  }
+  Query: {},
+  Mutation: {}
 };
 
 module.exports = resolvers;
--- src/schema.js
+++ src/schema.js
@@ -1,21 +1,5 @@
 const { gql } = require("apollo-server");
 
-const typeDefs = gql`
-  type User {
-    id: Int!
-    firstName: String!
-    lastName: String!
-    email: String!
-  }
-
-  type Query {
-    users: [User!]!
-    user(id: Int!): User
-  }
-
-  type Mutation {
-    createUser(firstName: String!, lastName: String!, email: String!): User!
-  }
-`;
+const typeDefs = gql``;
 
 module.exports = typeDefs;
