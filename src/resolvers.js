67b6fc23ec57f4582c95ba0cf407b7840f41829b
joshua-myers <100009956+joshua-myers@users.noreply.github.com>
2022-02-25 20:53:25 -0600

    Create url schema and models

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,6 +1,60 @@
+const validSlugKeys = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+const retrieveRandomKey = () => {
+  return validSlugKeys[Math.floor(Math.random() * validSlugKeys.length)];
+};
+
+const generateSlug = () => {
+  return [
+    retrieveRandomKey(),
+    retrieveRandomKey(),
+    retrieveRandomKey(),
+    retrieveRandomKey()
+  ].join();
+};
+
+const slugExists = async (models, slug) => {
+  return !!(
+    await models.Url.findOne({
+      where: {
+        slug
+      }
+    })
+  ).length;
+};
+
 const resolvers = {
-  Query: {},
-  Mutation: {}
+  Query: {
+    async url(root, { id }, { models }) {
+      return models.Url.findByPk(id);
+    },
+    async allUrls(root, args, { models }) {
+      return models.Url.findAll();
+    },
+    async urlBySlug(root, { slug }, { models }) {
+      return models.Url.findOne({
+        where: {
+          slug
+        }
+      });
+    }
+  },
+  Mutation: {
+    async createUrl(root, { url, slug }, { models }) {
+      if (!slug) {
+        // Generate a new unique slug
+        let slugExists = false;
+        do {
+          slug = generateSlug();
+          slugExists = slugExists(models, slug);
+        } while (!slugExists);
+      } else if (slugExists(models, slug)) {
+        return { error: { code: 1, message: "Duplicate Slug" } };
+      }
+
+      return models.Url.create(url, slug);
+    }
+  }
 };
 
 module.exports = resolvers;
--- src/schema.js
+++ src/schema.js
@@ -1,5 +1,21 @@
 const { gql } = require("apollo-server");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Url {
+    id: Int!
+    url: String!
+    slug: String!
+  }
+
+  type Query {
+    url(id: Int!): Url
+    urlBySlug(slug: String!): Url
+    allUrls: [Url!]!
+  }
+
+  type Mutation {
+    createUrl(url: String!, slug: String): Url!
+  }
+`;
 
 module.exports = typeDefs;
